edfd037f661a0e852d03eb8e7467eacd18c6fcdb
padr3vin <admin@docucheck.co.uk>
2025-07-31 22:39:22 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
